a97a979c3337eab83e77cfa7805e47b96fa2c1bd
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-05-30 19:39:21 +0200

    more object id tests

--- deno_lib/object_id_test.ts
+++ deno_lib/object_id_test.ts
@@ -4,8 +4,37 @@ import {
   assertEquals,
   assertThrows
 } from "https://deno.land/x/testing/asserts.ts";
-import { encode } from "./transcoding.ts";
+import { encode, decode} from "./transcoding.ts"
+import { serialize, deserialize } from "./bson.ts"
 import { ObjectId } from "./object_id.ts";
+
+const testVectors: { [key:string]: any} = JSON.parse(
+  decode(Deno.readFileSync("./../corpus/object_id_test_vectors.json"),"utf8")
+)
+
+testVectors.valid
+.forEach(({ description, canonical_bson, canonical_extjson}:  { [key:string]: string}): void => {
+  test({
+    name: description,
+    fn():void {
+      const expected_bson: Uint8Array = encode(canonical_bson, "hex")
+      const doc: { [key:string]: any} = deserialize(expected_bson)
+      const doc_extjson: string = JSON.stringify(doc.toExtendedJSON ? doc.toExtendedJSON() : doc)
+      // Reparsing from extended JSON bc of hardly controllable key order
+      assertEquals(JSON.parse(doc_extjson), JSON.parse(canonical_extjson))
+      assertEquals(serialize(doc), expected_bson)
+    }
+  })
+})
+
+testVectors.decodeErrors.forEach(({ description, bson }:  { [key:string]: string}): void => {
+  test({
+    name: description,
+    fn():void {
+      assertThrows(() => deserialize(encode(bson, "hex")))
+    }
+  })
+})
 
 test({
   name: "should correctly handle objectId timestamps",
