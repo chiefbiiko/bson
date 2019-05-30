8b86f38a90adefe96586ba8fe6665f58453f37b0
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-05-30 20:04:05 +0200

    timestamp fails

--- deno_lib/timestamp_test.ts
+++ deno_lib/timestamp_test.ts
@@ -2,6 +2,27 @@ import { test, runIfMain } from "https://deno.land/x/testing/mod.ts";
 import { assert, assertEquals } from "https://deno.land/x/testing/asserts.ts";
 import { MAX_VALUE, Timestamp } from "./timestamp.ts";
 import { MAX_UNSIGNED_VALUE, Long } from "./long/mod.ts";
+import { encode, decode} from "./transcoding.ts"
+import { serialize, deserialize } from "./bson.ts"
+
+const testVectors: { [key:string]: any} = JSON.parse(
+  decode(Deno.readFileSync("./../corpus/timestamp_test_vectors.json"),"utf8")
+)
+
+testVectors.valid
+.forEach(({ description, canonical_bson, canonical_extjson}:  { [key:string]: string}): void => {
+  test({
+    name: description,
+    fn():void {
+      const expected_bson: Uint8Array = encode(canonical_bson, "hex")
+      const doc: { [key:string]: any} = deserialize(expected_bson)
+      const doc_extjson: string = JSON.stringify(doc.toExtendedJSON ? doc.toExtendedJSON() : doc)
+      // Reparsing from extended JSON bc of hardly controllable key order
+      assertEquals(JSON.parse(doc_extjson), JSON.parse(canonical_extjson))
+      assertEquals(serialize(doc), expected_bson)
+    }
+  })
+})
 
 test({
   name: "should have a MAX_VALUE equal to MAX_UNSIGNED_VALUE of Long",
@@ -30,7 +51,7 @@ test({
   fn(): void {
     const timestamp: Timestamp = new Timestamp(0xffffffff, 0xffffffff);
     assertEquals(timestamp.toString(), "18446744073709551615");
-    assertEquals(timestamp.toJSON(), { $timestamp: "18446744073709551615" });
+    assertEquals(timestamp.toJSON(), timestamp.toExtendedJSON());
     assertEquals(timestamp.toExtendedJSON(), {
       $timestamp: { t: 4294967295, i: 4294967295 }
     });
